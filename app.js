80329e5ed5b58e9165c62d3a246ac99283987a2b
Josh <joshua.mcswain@mecklenburgcountync.gov>
2021-09-21 09:41:36 -0400

    for got to save before I pushed the las time

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+const profileDataArgs = process.argv.slice(2);
+
 const printProfileData = profileDataArr => {
- for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);  
- } 
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
+printProfileData(profileDataArgs);
