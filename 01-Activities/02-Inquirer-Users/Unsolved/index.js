9a4aa9f482b69a6699d94454ed8f724e4db9d2ba
Josh <joshua.mcswain@mecklenburgcountync.gov>
2021-09-23 09:50:16 -0400

    pushing changes I made will practicing with the code in 9.4 and 9.5

--- 01-Activities/02-Inquirer-Users/Unsolved/index.js
+++ 01-Activities/02-Inquirer-Users/Unsolved/index.js
@@ -1,28 +1,24 @@
 const fs = require('fs');
 // Install the `inquirer` dependency HERE
-const inquirer = require('inquirer');
 
 // Prompt the user for their name, preferred method of communication, and known languages
 // YOUR CODE HERE
 //
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
-    },
-    {
-      type: 'list',
-      name: 'communication',
-      message: 'What is your preferred method of communication?',
-      choices: ['email', 'phone', 'slack', 'carrier pigeon']
-    },
-    {
-      type: 'checkbox',
-      name: 'languages',
-      message: 'What languages do you know?',
-      choices: ['HTML', 'CSS', 'Javascript']
+
+// Write the user response to a file by chaining the below callback method to the prompt above.
+function(data) {
+
+  // Bonus: Generate the name of your user file from their input
+  // Your bonus code here
+  //
+
+  fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+
+    if (err) {
+      return console.log(err);
     }
-  ])
-  .then(function(data) {})
+
+    console.log("Success!");
+
+  });
+};
